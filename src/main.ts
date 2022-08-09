3f5f44038f81fa4ee7096418d4d83db680eef292
Tim Dillon <tim@itsdits.com>
2022-08-09 12:22:56 +0900

    chore: update paths

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { BuildPlugin } from '../@types';
 import { existsSync, copySync, pathExistsSync } from 'fs-extra';
 import path from 'path';
 
-const PACKAGE_NAME = 'Wortal';
+const PACKAGE_NAME = 'wortal';
 
 function log(...arg: any[]) {
     return console.log(`[${PACKAGE_NAME}] `, ...arg);
@@ -11,28 +11,25 @@ function log(...arg: any[]) {
 export const load: BuildPlugin.load = () => {
     log("ProjectPath:", Editor.Project.path);
 
-    const preview_template_dir = "preview";
-    const build_templates_dir = "build";
     const project_path = Editor.Project.path;
-    const templates_dir = path.join(Editor.Project.path, "extensions/" + PACKAGE_NAME + "/templates/");
     const assets_dir = path.join(project_path, "assets");
+    const api_dir = "wortal-api"
+    const build_dir = "build-templates";
+    const preview_dir = "preview-template";
+    const templates_dir = path.join(Editor.Project.path, "extensions/" + PACKAGE_NAME + "/templates/");
 
     const copy_list = [
         {
-            src: path.join(templates_dir, preview_template_dir),
-            dest: path.join(project_path, preview_template_dir)
+            src: path.join(templates_dir, preview_dir),
+            dest: path.join(project_path, preview_dir)
         },
         {
-            src: path.join(templates_dir, build_templates_dir),
-            dest: path.join(project_path, build_templates_dir)
+            src: path.join(templates_dir, build_dir),
+            dest: path.join(project_path, build_dir)
         },
         {
-            src: path.join(templates_dir, "wortal"),
-            dest: path.join(assets_dir, "wortal")
-        },
-        {
-            src: path.join(templates_dir, "demo"),
-            dest: path.join(assets_dir, "demo")
+            src: path.join(templates_dir, api_dir),
+            dest: path.join(assets_dir, api_dir)
         }
     ];
 
