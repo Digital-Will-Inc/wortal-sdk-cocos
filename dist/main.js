d6ea539b4cdb995c97a2ae7e03407a4a59b4ba40
Tim Dillon <tim@itsdits.com>
2022-08-25 10:53:46 +0900

    feat: add support for Cocos 3.6

--- dist/main.js
+++ dist/main.js
@@ -7,34 +7,67 @@ exports.methods = exports.unload = exports.load = void 0;
 const fs_extra_1 = require("fs-extra");
 const path_1 = __importDefault(require("path"));
 const PACKAGE_NAME = 'wortal-sdk';
-function log(...arg) {
-    return console.log(`[${PACKAGE_NAME}] `, ...arg);
-}
 exports.load = () => {
-    log("ProjectPath:", Editor.Project.path);
+    function log(...arg) {
+        return console.log(`[${PACKAGE_NAME}] `, ...arg);
+    }
+    function error(...arg) {
+        return console.error(`[${PACKAGE_NAME}] `, ...arg);
+    }
     const project_path = Editor.Project.path;
     const assets_dir = path_1.default.join(project_path, "assets");
     const api_dir = "wortal-api";
     const build_dir = "build-templates";
-    const templates_dir = path_1.default.join(Editor.Project.path, "extensions/" + PACKAGE_NAME + "/templates/");
-    const copy_list = [
+    const bridge_dir = "wortal-bridge";
+    const bridge_dest = "web-mobile/assets/js";
+    let version = "";
+    let editor = Editor.App.version;
+    log("Detected editor version: " + editor);
+    switch (editor) {
+        case "3.3.0":
+        case "3.3.1":
+        case "3.3.2":
+        case "3.4.0":
+        case "3.4.1":
+        case "3.4.2":
+        case "3.5.0":
+        case "3.5.1":
+        case "3.5.2":
+            version = "3.3";
+            break;
+        case "3.6.0":
+            version = "3.6";
+            break;
+        default:
+            error("Version not supported: " + editor);
+            break;
+    }
+    const static_templates = path_1.default.join(Editor.Project.path, "extensions/" + PACKAGE_NAME + "/templates/");
+    const versioned_templates = path_1.default.join(static_templates + version + "/");
+    const assets = [
         {
-            src: path_1.default.join(templates_dir, build_dir),
+            src: path_1.default.join(versioned_templates, build_dir),
             dest: path_1.default.join(project_path, build_dir)
         },
         {
-            src: path_1.default.join(templates_dir, api_dir),
+            src: path_1.default.join(static_templates, api_dir),
             dest: path_1.default.join(assets_dir, api_dir)
+        },
+        {
+            src: path_1.default.join(static_templates, bridge_dir),
+            dest: path_1.default.join(project_path, build_dir, bridge_dest)
         }
     ];
-    copy_list.forEach((value) => {
+    log("Copying assets..");
+    assets.forEach((value) => {
         if (fs_extra_1.pathExistsSync(value.dest) == false) {
             fs_extra_1.copySync(value.src, value.dest);
         }
         else {
-            console.log("Skipping:", value.dest);
+            log("Skipping asset: ", value.dest);
         }
     });
+    log("Asset copying complete.");
 };
 exports.unload = () => {
     console.log("[Wortal] Extension disabled.");
--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,80 @@
-import { BuildPlugin } from '../@types';
-import { existsSync, copySync, pathExistsSync } from 'fs-extra';
+import {BuildPlugin} from '../@types';
+import {copySync, pathExistsSync} from 'fs-extra';
 import path from 'path';
 
 const PACKAGE_NAME = 'wortal-sdk';
 
-function log(...arg: any[]) {
-    return console.log(`[${PACKAGE_NAME}] `, ...arg);
-}
-
 export const load: BuildPlugin.load = () => {
-    log("ProjectPath:", Editor.Project.path);
+    function log(...arg: any[]) {
+        return console.log(`[${PACKAGE_NAME}] `, ...arg);
+    }
+
+    function error(...arg: any[]) {
+        return console.error(`[${PACKAGE_NAME}] `, ...arg);
+    }
 
     const project_path = Editor.Project.path;
     const assets_dir = path.join(project_path, "assets");
-    const api_dir = "wortal-api"
+    const api_dir = "wortal-api";
     const build_dir = "build-templates";
-    const templates_dir = path.join(Editor.Project.path, "extensions/" + PACKAGE_NAME + "/templates/");
+    const bridge_dir = "wortal-bridge";
+    const bridge_dest = "web-mobile/assets/js";
+    let version = "";
+    let editor = Editor.App.version;
 
-    const copy_list = [
+    log("Detected editor version: " + editor);
+
+    switch (editor) {
+        case "3.3.0":
+        case "3.3.1":
+        case "3.3.2":
+        case "3.4.0":
+        case "3.4.1":
+        case "3.4.2":
+        case "3.5.0":
+        case "3.5.1":
+        case "3.5.2":
+            version = "3.3";
+            break;
+        case "3.6.0":
+            version = "3.6";
+            break;
+        default:
+            error("Version not supported: " + editor);
+            break;
+    }
+
+    const static_templates = path.join(Editor.Project.path, "extensions/" + PACKAGE_NAME + "/templates/");
+    const versioned_templates = path.join(static_templates + version + "/");
+
+    const assets = [
         {
-            src: path.join(templates_dir, build_dir),
+            src: path.join(versioned_templates, build_dir),
             dest: path.join(project_path, build_dir)
         },
         {
-            src: path.join(templates_dir, api_dir),
+            src: path.join(static_templates, api_dir),
             dest: path.join(assets_dir, api_dir)
+        },
+        {
+            src: path.join(static_templates, bridge_dir),
+            dest: path.join(project_path, build_dir, bridge_dest)
         }
     ];
 
-    copy_list.forEach((value: { src: string, dest: string }) => {
+    log("Copying assets..")
+    assets.forEach((value: { src: string, dest: string }) => {
         if (pathExistsSync(value.dest) == false) {
-            copySync(value.src, value.dest)
+            copySync(value.src, value.dest);
         } else {
-            console.log("Skipping:", value.dest)
+            log("Skipping asset: ", value.dest);
         }
     });
+    log("Asset copying complete.")
 };
 
 export const unload: BuildPlugin.Unload = () => {
-    console.log("[Wortal] Extension disabled.")
+    console.log("[Wortal] Extension disabled.");
 };
 
 export const methods: { [key: string]: (...any: any) => any } = {
