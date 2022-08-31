42eb26eeb21889cae33e8f656606d161cc343b0b
Tim Dillon <tim@itsdits.com>
2022-08-31 14:00:29 +0900

    refactor: use compare-versions instead of ugly switch

--- dist/main.js
+++ dist/main.js
@@ -3,8 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.methods = exports.unload = exports.load = void 0;
+exports.unload = exports.load = void 0;
 const fs_extra_1 = require("fs-extra");
+const compare_versions_1 = require("compare-versions");
 const path_1 = __importDefault(require("path"));
 const PACKAGE_NAME = 'wortal-sdk';
 exports.load = () => {
@@ -20,34 +21,20 @@ exports.load = () => {
     const build_dir = "build-templates";
     const bridge_dir = "wortal-bridge";
     const bridge_dest = "web-mobile/assets/js";
+    const demo_dir = "wortal-demo";
     let version = "";
     let editor = Editor.App.version;
     log("Detected editor version: " + editor);
-    switch (editor) {
-        case "3.0.0":
-        case "3.0.1":
-        case "3.1.0":
-        case "3.1.1":
-        case "3.1.2":
-        case "3.2.0":
-        case "3.2.1":
-        case "3.3.0":
-        case "3.3.1":
-        case "3.3.2":
-        case "3.4.0":
-        case "3.4.1":
-        case "3.4.2":
-        case "3.5.0":
-        case "3.5.1":
-        case "3.5.2":
-            version = "3.0";
-            break;
-        case "3.6.0":
-            version = "3.6";
-            break;
-        default:
-            error("Version not supported: " + editor);
-            break;
+    // Versions 3.0.0 - 3.5.2 should use the 3.0 templates. 3.6+ uses the 3.6 template.
+    // This is due to major changes in the build template starting in 3.6.0.
+    if (compare_versions_1.compare(editor, '3.0.0', '>=') && compare_versions_1.compare(editor, '3.5.2', '<=')) {
+        version = "3.0";
+    }
+    else if (compare_versions_1.compare(editor, '3.6.0', '>=')) {
+        version = "3.6";
+    }
+    else {
+        error("Version not supported: " + editor);
     }
     const static_templates = path_1.default.join(Editor.Project.path, "extensions/" + PACKAGE_NAME + "/templates/");
     const versioned_templates = path_1.default.join(static_templates + version + "/");
@@ -63,6 +50,10 @@ exports.load = () => {
         {
             src: path_1.default.join(static_templates, bridge_dir),
             dest: path_1.default.join(project_path, build_dir, bridge_dest)
+        },
+        {
+            src: path_1.default.join(static_templates, demo_dir),
+            dest: path_1.default.join(assets_dir, demo_dir)
         }
     ];
     log("Copying assets..");
@@ -76,9 +67,5 @@ exports.load = () => {
 };
 exports.unload = () => {
     console.log("[Wortal] Extension disabled.");
-};
-exports.methods = {
-    openPanel() {
-        Editor.Panel.open(PACKAGE_NAME);
-    },
+    //TODO: safely remove assets that were copied into project
 };
