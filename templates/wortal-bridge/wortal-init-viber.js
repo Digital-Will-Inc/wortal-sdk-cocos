2cddfe6d467067aec7cdf32f1e149f618e5cc6c5
Tim Dillon <tim@itsdits.com>
2022-10-03 14:23:45 +0900

    fix: possible null ref in link/viber init (#71)

--- templates/wortal-bridge/wortal-init-viber.js
+++ templates/wortal-bridge/wortal-init-viber.js
@@ -2,8 +2,10 @@ window.addEventListener("load", () => {
     window.initWortal(function () {
         console.log("[Wortal] Setup complete.");
         document.getElementById("loading-cover").style.display = "none";
-        window.wortalGame.initializeAsync().then(() => {
-            window.wortalGame.startGameAsync();
-        });
+        if (window.wortalGame) {
+            window.wortalGame.initializeAsync().then(() => {
+                window.wortalGame.startGameAsync();
+            });
+        }
     });
 });
