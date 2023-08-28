9830356e3d39b6630a35925028fafc30d5524126
Tim Dillon <tim@itsdits.com>
2023-08-28 15:26:11 +0900

    feat(core): add setLoadingProgress api

--- templates/wortal-api/Wortal.ts
+++ templates/wortal-api/Wortal.ts
@@ -88,6 +88,23 @@ export class Wortal {
     }
 
     /**
+     * Sets the loading progress value for the game. This is required for the game to start. Failure to call this with 100
+     * once the game is fully loaded will prevent the game from starting.
+     * @example
+     * onGameLoadProgress(percent) {
+     *     Wortal.setLoadingProgress(percent);
+     * }
+     *
+     * onGameLoaded() {
+     *     Wortal.setLoadingProgress(100);
+     * }
+     * @param value Percentage of loading complete. Range is 0 to 100.
+     */
+    setLoadingProgress(value: number): void {
+        (window as any).Wortal.setLoadingProgress(value);
+    }
+
+    /**
      * Sets a callback which will be invoked when the app is brought to the background.
      * @param callback Callback to invoke.
      */
